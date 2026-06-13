44146a6026e2a0b6162578ac130a7d00a939172e
Ghost <test@gmail.com>
2026-06-13 19:55:28 +0530

    updating docker commands

--- studentsystem/studentsystem/settings.py
+++ studentsystem/studentsystem/settings.py
@@ -75,7 +75,7 @@ ROOT_URLCONF = 'studentsystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': os.environ.get('TEMPLATE_DIRS', [BASE_DIR / 'templates']),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,9 +135,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = os.environ.get('STATIC_URL', '/static/')
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = os.environ.get('STATICFILES_DIRS', [BASE_DIR / 'static'])
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
